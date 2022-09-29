59555f4bac2c1d4ee758a6f088932fd63f8dc2c0
Rosanna Zhang <rosannazhang92@gmail.com>
2022-09-28 18:28:50 -0700

    Function that checks for the specified word vertically and horizontally, returns true/false

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Word Search - looks into the word search to find the given word
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+// if word search are empty arrays
+  if (letters === []) {
+    return false;
+  }
+
+  // checks for the word horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+    }
+// pushes the vertical letters into a new array, joins it, checks it 
+  for (let row = 0; row < horizontalJoin.length; row++) {
+    let array = [];
+    for (let col = 0; col < horizontalJoin[row].length; col++) {
+      array.push(horizontalJoin[col][row])
+        } 
+        let newArray = array.join('');
+        if (newArray.includes(word)) {
+          return true;
+        }
+      }
+    return false;
+  };
+
+
+module.exports = wordSearch;
+
